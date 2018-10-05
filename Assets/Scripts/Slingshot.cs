83c62fedc79e782ca9fb0a558b3b3d9601678f50
Von Arzu <vonarzu@gmail.com>
2018-10-04 22:39:12 -0400

    Slingshot script
    Updated the mouse aiming and up and down functions

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -6,6 +6,7 @@ public class Slingshot : MonoBehaviour
 {
     [Header("Set in inspector")]
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
 
     [Header("Set Dynamically")]
     public GameObject launchPoint;
@@ -13,11 +14,15 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
 
-    private void Awake()
+    private Rigidbody projectileRigidbody;
+
+
+    void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
     void OnMouseEnter()
     {
@@ -37,5 +42,34 @@ public class Slingshot : MonoBehaviour
         projectile.transform.position = launchPos;
         projectile.GetComponent< Rigidbody > ().isKinematic = true;
 
+    }
+   void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if(mouseDelta.magnitude > maxMagnitude){
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if( Input.GetMouseButtonUp (0) ){
+
+            aimingMode = false;
+           projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+
+        }
+
+        
     }
 }

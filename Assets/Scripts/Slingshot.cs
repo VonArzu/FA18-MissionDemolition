9eeec2e055e99f6866e057e8f66b5d47abe4bf18
Von Arzu <vonarzu@gmail.com>
2018-10-04 21:18:54 -0400

    Continuation of C# script
    Further in the chapter with scripts

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slingshot : MonoBehaviour {
+public class Slingshot : MonoBehaviour
+{
+    [Header("Set in inspector")]
+    public GameObject prefabProjectile;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        //print("Slingshot: OnMouseEnter ()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate( prefabProjectile ) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent< Rigidbody > ().isKinematic = true;
+
+    }
 }
